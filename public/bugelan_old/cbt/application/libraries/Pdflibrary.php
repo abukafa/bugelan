2a3d6e50a5e54f8794554b6f7323f636d099fabc
abukafa <hikam.abukafa@gmail.com>
2023-08-04 19:53:06 +0700

    make sure to ready redeploy

--- public/bugelan_old/cbt/application/libraries/Pdflibrary.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Pdflibrary
-{
-    function __construct()
-    {
-        include_once APPPATH . '/third_party/fpdf/fpdf.php';
-    }
-}
--- public/bugelan_old/cbt/application/models/Modelsiswa.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Modelsiswa extends CI_Model
-{
-    function tampil_data_siswa()
-    {
-        return $this->db->get('m_siswa');
-    }
-}
